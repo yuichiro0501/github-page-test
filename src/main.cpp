0f463c33edb3c083fb21e9dc3094b7dacc27a8ad
yuichiro0501 <yuichiro.nakahta@outlook.jp>
2023-04-22 23:49:28 +0900

    add test config in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -21,11 +21,22 @@ int main()
   /* Set line */
   Line line(pt, gradient);
 
+
+  /* Prepair test */
+  double x_res, y_res;
+
   /* Get x coord when y = 0 */
-  double x_res = line.get_x(0.0);
+  x_res = line.get_x(0.0);
   std::cout << "x = " << x_res << ", when y = 0" << std::endl;
   /* Get y coord when x = 0 */
-  double y_res = line.get_y(0.0);
+  y_res = line.get_y(0.0);
+  std::cout << "y = " << x_res << ", when x = 0" << std::endl;
+
+  /* Get x coord when y = 1.0 */
+  x_res = line.get_x(1.0);
+  std::cout << "x = " << x_res << ", when y = 0" << std::endl;
+  /* Get y coord when x = 1.0 */
+  y_res = line.get_y(1.0);
   std::cout << "y = " << x_res << ", when x = 0" << std::endl;
 
   return 0;
